e7f4a6b382a0ff05439b2018e958ee220aeb5633
Vipul Sharma <vipulsh@kth.se>
2025-08-01 21:07:37 +0200

    Deleted Project v3. No use

--- SPC58EC - Combined_Application_v3/.dep/crt0.o.d
+++
@@ -1,8 +0,0 @@
-build/obj/crt0.o: \
- components/spc58ecxx_platform_component_rla/lib/src/crt0.s \
- components/spc58ecxx_platform_component_rla/cfg/boot.h \
- components/spc58ecxx_platform_component_rla/cfg/toolchain.h
-
-components/spc58ecxx_platform_component_rla/cfg/boot.h:
-
-components/spc58ecxx_platform_component_rla/cfg/toolchain.h:
